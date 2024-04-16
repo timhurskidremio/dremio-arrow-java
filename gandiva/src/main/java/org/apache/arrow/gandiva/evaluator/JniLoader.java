1c7722fdd653938f42b36da475060e2591100948
DenisTarasyuk <denis.tarasyuk@dremio.com>
2024-04-16 19:09:52 +0300

    DX-88565 Fixed gandiva native library loading option to be global
    Added jna library which allows more control over native code loading

--- gandiva/src/main/java/org/apache/arrow/gandiva/evaluator/JniLoader.java
+++ gandiva/src/main/java/org/apache/arrow/gandiva/evaluator/JniLoader.java
@@ -23,6 +23,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.Collections;
 import java.util.Locale;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -31,6 +32,9 @@ import org.apache.arrow.gandiva.exceptions.GandivaException;
 /** This class handles loading of the jni library, and acts as a bridge for the native functions. */
 class JniLoader {
   private static final String LIBRARY_NAME = "gandiva_jni";
+  
+  private static final int RTLD_GLOBAL = 0x00100;
+  private static final int RTLD_LAZY = 0x00001; 
 
   private static volatile JniLoader INSTANCE;
   private static volatile long defaultConfiguration = 0L;
@@ -69,6 +73,10 @@ class JniLoader {
     final String libraryToLoad =
         LIBRARY_NAME + "/" + getNormalizedArch() + "/" + System.mapLibraryName(LIBRARY_NAME);
     final File libraryFile = moveFileFromJarToTemp(tmpDir, libraryToLoad, LIBRARY_NAME);
+    NativeLibrary.getInstance(
+        libraryFile.getAbsolutePath(),
+        Collections.singletonMap(Library.OPTION_OPEN_FLAGS, new Integer(RTLD_LAZY | RTLD_GLOBAL))
+    );
     System.load(libraryFile.getAbsolutePath());
   }
 
